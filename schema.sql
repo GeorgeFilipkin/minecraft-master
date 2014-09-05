2759778b333e25f1549d6eced314ae9404e40a72
krotebot <krotebot@gmail.com>
2014-09-05 23:01:55 +0400

    readme

---
+++ schema.sql
@@ -0,0 +1,106 @@
+-- MySQL dump 10.13  Distrib 5.5.37, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: master
+-- ------------------------------------------------------
+-- Server version	5.5.37-1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `banned_players`
+--
+
+DROP TABLE IF EXISTS `banned_players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `banned_players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player` varchar(32) DEFAULT NULL,
+  `reason` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `who_banned` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `banned_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `player` (`player`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ids`
+--
+
+DROP TABLE IF EXISTS `ids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ids` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player` varchar(32) DEFAULT NULL,
+  `ip` varchar(40) DEFAULT NULL,
+  `ticket` varchar(64) DEFAULT NULL,
+  `launcher_ver` varchar(32) DEFAULT NULL,
+  `os` varchar(32) DEFAULT NULL,
+  `os_arch` varchar(32) DEFAULT NULL,
+  `os_version` varchar(64) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=368 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player` varchar(32) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `salt` varchar(32) DEFAULT NULL,
+  `ismod` tinyint(1) DEFAULT '0',
+  `clientToken` varchar(64) DEFAULT NULL,
+  `accessToken` varchar(64) DEFAULT NULL,
+  `serverId` varchar(64) DEFAULT NULL,
+  `registered_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `player` (`player`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `unbanned_players`
+--
+
+DROP TABLE IF EXISTS `unbanned_players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `unbanned_players` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `player` varchar(32) DEFAULT NULL,
+  `reason` varchar(256) CHARACTER SET utf8 DEFAULT NULL,
+  `who_unbanned` varchar(64) CHARACTER SET utf8 DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-09-05 14:37:15
